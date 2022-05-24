14d3738f1b7defcb4db0201c952d814c7d26dd40
Hyun Sung Cho <hcho774@gmail.com>
2022-05-23 22:49:12 -0400

    commit tree 7a3d87cdfdbe8a563942351d67bac17144f945ae

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
+  attr_reader :a, :b, :c
   # write code here
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    validate_triangle
+    if a == b && b == c    
+      :equilateral
+    elsif a == b || b == c || a == c 
+      :isosceles
+      
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+      a > 0 && b > 0 && c > 0
+  end
+
+  def triangle_inequality?
+    a + b > c && b + c > a && c + a > b
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && triangle_inequality?
+  end
+
+  
+  class TriangleError < StandardError
+  end
+
 end
+
